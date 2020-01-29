e5f56fa666bad73386a3c5bb09e614731f2696e6
RomilleReyes <1719415@brunel.ac.uk>
2020-01-29 22:17:28 +0000

    Added likebutton to like question

--- app/src/main/java/com/example/cs2001gp/MainActivity.java
+++ app/src/main/java/com/example/cs2001gp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.cs2001gp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int likesCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void on_likebutton_click(View view){
+        incrementLikes(likesCounter);
+    }
+
+    public static int incrementLikes(int currentQnScore) {
+        currentQnScore += 1;
+        return currentQnScore;
     }
 }

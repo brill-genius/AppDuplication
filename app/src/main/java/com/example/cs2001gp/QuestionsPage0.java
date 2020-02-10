6dccc7be7a76a1d86b22be14cd6e8d3c58dc20c2
1815987@brunel.ac.uk <1815987@brunel.ac.uk>
2020-02-10 23:34:15 +0000

    Set names and linked variables to objects
    Signed-off-by: 1815987@brunel.ac.uk <1815987@brunel.ac.uk>

--- app/src/main/java/com/example/cs2001gp/QuestionsPage0.java
+++ app/src/main/java/com/example/cs2001gp/QuestionsPage0.java
@@ -3,12 +3,28 @@ package com.example.cs2001gp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class QuestionsPage0 extends AppCompatActivity {
+    EditText et;
+    Button bt;
+    ListView lv;
+    ArrayList<String> arraylist;
+    ArrayAdapter<String> Adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page0);
+
+        et = (EditText)findViewById(R.id.Questiontxt);
+        bt = (Button)findViewById(R.id.Submitbtn);
+        lv = (ListView)findViewById(R.id.Questionlv);
+
     }
 }

e92373dbf9214889a1c8d6b714289dc9501b258c
1815987@brunel.ac.uk <1815987@brunel.ac.uk>
2020-02-10 23:19:38 +0000

    Interactive Buttons added and other design features
    Signed-off-by: 1815987@brunel.ac.uk <1815987@brunel.ac.uk>

---
+++ app/src/main/java/com/example/cs2001gp/QuestionsPage0.java
@@ -0,0 +1,14 @@
+package com.example.cs2001gp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionsPage0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_page0);
+    }
+}
